15242dd089afec5c0f5e27b935b41c7e7b8339cc
kevin david cuadros <kevindavidcuadros@gmail.com>
2020-12-23 23:51:09 -0500

    variables env MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev' ){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://cuadros:mfOGVX1wID1acspd@cluster0.htiey.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URL
 }
 
 process.env.URLDB = urlDB;
